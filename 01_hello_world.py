5d7ad4774ae56a418a736869b99d951612474b36
rahul1387 <rahultripathi7447@gmail.com>
2023-10-20 11:45:37 +0530

    Print format and Modules.

---
+++ 01_hello_world.py
@@ -0,0 +1,1 @@
+print("Hello World"); # ---> This is the format to print in python
---
+++ modulles.py
@@ -0,0 +1,8 @@
+import playsound
+# This is how we can import a module in our code
+''' Modules are of two type: 
+                            1. Build in modules --> which is pre installed in vs code
+                            2. Externel modules --> which we have to download from terminal
+    We can install modules using command pip install moduleName
+    Type this in terminal
+'''
