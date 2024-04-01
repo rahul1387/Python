971b78fca8acc0e7aafcb71844aece0f401208d7
rahul1387 <rahultripathi7447@gmail.com>
2024-04-01 18:55:17 +0530

    Variables and Data types.

--- 02_variables.py
+++ 02_variables.py
@@ -1,1 +1,29 @@
-a = "Rahul";
+a = "Rahul"; #This is string data type
+b = 1; # This is integer type
+c = True; # This is bollean data type
+d = None; #This is none data type
+
+#Data type is used to store a value in memory.
+
+# to determine type of variable
+print("The type of a is" , type(a))
+
+
+# Some more data types
+# Number Data: int, float, complex
+
+num1 = 13; #----> int
+num2 = 1.1; #----> float
+num3 = complex(8, 2); #----> COMPLEX
+print(num3)
+
+# 2nd Data type is Sequence Data: list, tuple
+
+list1 = [8, 2.3, [-4,5], ["apple", "banana"]]; # list is ordered pair of data with elements seperated by comma.THESE ARE MUTABLE!
+print(list1);
+
+
+# Tuple ---> # list is ordered pair of data with elements seperated by comma.THESE ARE IMMUTABLE!
+
+tuple1 = (8, 2.3, (-4,5), ("apple", "banana"));
+print(tuple1);
