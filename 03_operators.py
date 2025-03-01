89613a5cc50f5c88d19129d4d649ab59b254749a
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-03-01 21:26:32 +0530

    Operators in python

---
+++ 03_operators.py
@@ -0,0 +1,14 @@
+# Opeartors are simple maths like : addition, multiplication, division, minus, But in python there some more operators like  modulus, floor Division, Exponential,
+
+num1 = 5;
+num2 = 3;
+
+# modulus denoted by (%)
+print(num1%num2);
+
+# Floor Division denoted by (//)
+print(num1//num2);
+
+# modulus denoted by (**)
+print(num1**num2);
+
