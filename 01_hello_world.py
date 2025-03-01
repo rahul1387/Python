ceb7861dd029568ac7cbd9adee43d25c66eedd2c
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-03-01 21:25:27 +0530

    Basic python

---
+++ 01_hello_world.py
@@ -0,0 +1,20 @@
+print("Hello World"); # ---> This is the format to print in python
+a = 54;
+print("Rahul", a); # ---> This is concatination in python.
+
+# Multiline Comments
+'''
+    This is multiple lines comments
+    you can see
+    no error
+'''
+
+# Escape Sequence Character
+
+print("Hey I'm a \"good boy\"");
+# There are many ESC like \n (for new line gaap) 
+
+# More about print statement
+
+print("DOB :", 1,1,2001, sep="/") # In this print statement sep parameter put / after every element 
+print("Holla" , end="Petter")
