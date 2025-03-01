36b1b6160f3f9d831573ea787d60520299629096
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-03-01 21:26:53 +0530

    Type Casting

---
+++ 04_typeCasting.py
@@ -0,0 +1,17 @@
+a = "1";
+b = "2";
+
+# print(a + b);
+
+''' A convertion of one data type into another data type is known as Cype Casting.
+ There are various functions or methods like int(), float(), str(),ord(),list(),dict(),hex(), etc.
+Tow types of typecasting :  1.Explict typeCasting (Applied by programmer)
+                            2.Implict typeCasting (Applied by Python)
+'''                           
+print(int(a) + int(b));  # but if we add any letters in a or b anfd tell it to add then the error will occur like:
+
+# Implict Typecasting
+num1 = 2;
+num2 = 3.3;
+
+print(num1 + num2);
