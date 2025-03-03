98c7105c6a8fef50abd84b6fefbb4c119c1a46c1
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-03-03 22:38:03 +0530

    Operations and slicing of string

---
+++ 07_operationOnStrings.py
@@ -0,0 +1,13 @@
+fruit = "Mango"
+
+# Length of string
+mangoLen = len(fruit)
+print(mangoLen)
+
+# Print string letter by giving the ranges of indexes
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[0:len(fruit)-3])
+print(fruit[-1:-3])
