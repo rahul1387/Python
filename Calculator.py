46f804f518a27827c5dc613c53e086564c7fab34
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-06-29 22:20:36 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,6 @@
 
 Num1 = input("Enter first number: ")
+
 Num2 = input("Enter second number: ")
 
 print("The sum of two numbers is: " + Num1 + Num2)
@@ -11,3 +12,4 @@ print("The product of two number is: " + Num1*Num2)
 print("The division of two number is: " + Num1/Num2)
 
 
+print("The remainder of two numbers is: " + Num1 % Num2)
