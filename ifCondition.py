931f209c2087086b49c65a9601d46569b26cb719
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-07-02 22:03:48 +0530

    Update ifCondition.py

--- ifCondition.py
+++ ifCondition.py
@@ -4,3 +4,15 @@ if(age>=18):
     print("You can drive!")
 else:
     print("You cannot drive")
+
+
+applePrice = int(input("Enter the apple price: "))
+budget = int(input("Enter your budget: "))
+
+    
+if(budget - applePrice > 50):
+        print("Alexa, add 1kg apples to the card.")
+elif(budget - applePrice > 70):
+        print("Its okay you can buy ")
+else:
+        print("Alexa, do not add apples to the card.")
