9fda18dbc7a0a369e144ab2660e4f1118cf4ab38
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-07-07 22:43:43 +0530

    Update loops.py
    loops in python

--- loops.py
+++ loops.py
@@ -11,3 +11,18 @@ for k in range(1,9):
 
 for k in range(1 , 12 , 3):
     print(k)    
+
+
+# While loop
+
+i = int(input("Enter the number: "))
+while(i<=38):
+    i = int(input("Enter the number: "))
+    print(i)
+
+print("Done with the loop ")        
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count - 1
