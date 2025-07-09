d27460e303321c5e660e3bde3ba749e91b9c98ad
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-07-09 16:26:04 +0530

    Update 05_takingInput.py
    Input function in python.

--- 05_takingInput.py
+++ 05_takingInput.py
@@ -6,6 +6,6 @@ print("Hello", a,"!!")
 =======
 # There is a function to take input from user.
 
-a = input("Pls enter your name here: ");
+a = input("Pls enter your name here: "); 
 print("Hello", a,"!!!")
 >>>>>>> 3f1f80707164c689b95dbc0231141cc3b0f97276
