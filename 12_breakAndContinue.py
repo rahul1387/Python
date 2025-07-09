f826f75dd9d5fd308bc1320aaa438f9e5d9def42
rahul1387 <rahultripathi7447@gmail.com>
2025-07-09 16:35:23 +0530

    Break and Continue statements.

--- 12_breakAndContinue.py
+++ 12_breakAndContinue.py
@@ -1,5 +1,16 @@
+
+# Continue
 for i in range(12):
     if(i == 10):
         print("Skip the iteration")
         continue
     print("5 X", i, "=", 5*i)
+
+# Break
+
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break # ---> It will break the loop even if the condition of while is true.
