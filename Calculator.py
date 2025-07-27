79e4024b25265016b40f07a477722498c3efabef
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-07-27 19:55:28 +0530

    calculator using python

--- Calculator.py
+++ Calculator.py
@@ -6,7 +6,7 @@ print("The sum of two numbers is: " + Num1 + Num2)
  
 print("The difference of two numbers is: " + Num1 - Num2)
 
-print("The product of two number is: " + Num1*Num2)
+print("The product of two number is: " + Num1*Num2) 
 
 print("The division of two number is: " + Num1/Num2)
 
