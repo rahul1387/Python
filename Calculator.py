ea6a5ee57222929509ee370dba10017e9dec6120
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-07-30 21:01:28 +0530

    Calculator using python

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-Num1 = input("Enter first number: ") 
+Num1 = input("Enter first number: ")
 
 Num2 = input("Enter second number: ")
 
@@ -6,7 +6,7 @@ print("The sum of two numbers is: " + Num1 + Num2)
  
 print("The difference of two numbers is: " + Num1 - Num2)
 
-print("The product of two number is: " + Num1 * Num2) 
+print("The product of two number is: " + Num1 * Num2)
 
 print("The division of two number is: " + Num1 / Num2)
 
